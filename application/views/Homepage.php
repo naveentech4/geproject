718ab5d1410d0cbadfba8c64b9e526a228c28744
naveentech4 <lingolanaveenkumar@gmail.com>
2023-06-14 22:21:05 +0530

    test

--- application/views/Homepage.php
+++ application/views/Homepage.php
@@ -12,7 +12,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   <body>
    <div class="jumbotron text-center">
      <h1>Bhavan's Vivekananda College</h1>
-     <h3>SEC3</h3>
+     <h3>Iam changed</h3>
    </div>
    <table>
    <?php $sucess=$this->session->flashdata('inserted');
